5e7880b681751a43bb9e5d8721efada630f24576
NICK_THUCYDIDES <nicholas.thucydides@gmail.com>
2025-10-19 19:27:08 -0700

    update dbconfig

--- server2/dbconfig.js
+++ server2/dbconfig.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const DB_CONFIG = {
   host: process.env.DB_HOST || 'localhost',
-  port: 3306,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
